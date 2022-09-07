80f9d1b2b411bcec9d310150ffead1a52cd0e2e3
Masha Kuznetsova <kmp2535@yandex.ru>
2022-09-07 14:58:04 +0700

    project 13

--- routes/cards.js
+++ routes/cards.js
@@ -4,10 +4,10 @@ const {
   createCard, sendCardsData, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-cardRouter.get('/cards', sendCardsData);
-cardRouter.post('/cards', createCard);
 cardRouter.delete('/cards/:cardId', deleteCard);
 cardRouter.put('/cards/:cardId/likes', likeCard);
 cardRouter.delete('/cards/:cardId/likes', dislikeCard);
+cardRouter.get('/cards', sendCardsData);
+cardRouter.post('/cards', createCard);
 
 module.exports = cardRouter;
--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,10 @@ const {
   createUser, getUsers, getUser, updateProfile, updateAvatar,
 } = require('../controllers/users');
 
-userRouter.post('/users', createUser);
-userRouter.get('/users', getUsers);
 userRouter.get('/users/:userId', getUser);
 userRouter.patch('/users/me', updateProfile);
 userRouter.patch('/users/me/avatar', updateAvatar);
+userRouter.post('/users', createUser);
+userRouter.get('/users', getUsers);
 
 module.exports = userRouter;
